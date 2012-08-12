676d8b0b87539ed5e343e36c6a915489cb4d2daf
Nyomancer <nyomancer@gmail.com>
2012-08-12 14:57:24 +0200

    removed whitespace chaos

--- lala/plugins/roulette.py
+++ lala/plugins/roulette.py
@@ -4,32 +4,26 @@ from lala.util import command, msg
 class revolver:
     def __init__(self):
         self.bullet = 1
-	self.chamber = 1
-	self.blasted = True
-    
+        self.chamber = 1
+        self.blasted = True
 
     def reload(self, user, channel, text):
         self.bullet = random.randint(1,6)
-	self.chamber = 1
-	self.blasted = False
-	msg(channel, "Can't you see I'm reloading")
+        self.chamber = 1
+        self.blasted = False
+        msg(channel, "Can't you see I'm reloading")
 
     def shoot(self, user, channel, text):
         if (self.chamber > 6) or self.blasted:
-            #output "Please reload"
-	    msg(channel, "Please reload")
-	 
-	elif (self.chamber == self.bullet):
-            #ouput "BOOM"
-	    msg(channel, "%s: BOOM" % user)
-	    self.blasted = True
-	    
-	else:
-	    #ouput "click"
-	    msg(channel, "%s: *click*" % user)
-	    self.chamber += 1
-	    
+            msg(channel, "Please reload")
 
+        elif (self.chamber == self.bullet):
+            msg(channel, "%s: BOOM" % user)
+            self.blasted = True
+
+        else:
+            msg(channel, "%s: *click*" % user)
+            self.chamber += 1
 
 knarre = revolver()
 
